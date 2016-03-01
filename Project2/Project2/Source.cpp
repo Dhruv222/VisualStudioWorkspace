4e0393aab1c0c27bb69a42a2346c7f25ddfcf927
Dhruv Sharma <dhruv004@e.ntu.edu.sg>
2016-03-01 22:54:26 +0530

    CountStepsinMatrix_CodeChef_Project2

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -2,60 +2,46 @@
 #include<vector>
 #include<string>
 #include<sstream>
+#include<math.h>
+#include<stdlib.h>
 
 using namespace std;
 
-int CalSubArrays(vector<int> TestCase, int count) {
+struct matrix
+{
+	int i, j;
 
-	vector<int> SubArray;
-	int output;
-	output = count;
-	int k;
-	for (int i = 2;i <= count;i++) {
-		for (int j = 0;j < count - i + 1;j++) {
-			SubArray.assign(TestCase.begin() + j, TestCase.begin() + j + i);
-			for (k = 0;k < SubArray.size()-1;k++)
-			{
-				if (SubArray[k] > SubArray[k + 1]) {
-					break;
-				}
-				
+};
+
+int main()
+{
+	int n=1,j=0,t=1;
+	cin >> t;
+	vector<matrix> data;
+	string row;
+	int item;
+	vector<int> output;
+	output.resize(t);
+	for (int l = 0;l < t;l++) {
+		cin >> n;
+		data.resize(n*n + 1);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0;j < n;j++) {
+				cin >> item;
+				data[item].i = i;
+				data[item].j = j;
 			}
-			if (k == SubArray.size() - 1)
-				output += 1;
+		}
+		for (int k = 1; k < n*n; k++) {
+			output[l]+=abs(data[k].i - data[k + 1].i) + abs(data[k].j - data[k + 1].j);
 		}
 	}
-	return output;
-}
-
-void main()
-{
-	
-	int noTestCases=1;
-	cin >> noTestCases;
-	vector<int> lenTestCases;
-	lenTestCases.resize(noTestCases);
-	string TestCaseString;  
-	vector<int> TestCase;
-	string number;
-	vector<int> output;
-
-	for (int i = 0; i < noTestCases; i++) {
-		cin >> lenTestCases[i];
-		TestCase.clear();
-		for (int j = 0; j < lenTestCases[i]; j++) {
-			cin >> TestCaseString;
-			
-			stringstream iss(TestCaseString);
-			while (getline(iss, number, ' ')) {
-				TestCase.push_back(stoi(number));
-			}	
-		} 
-		output.push_back(CalSubArrays(TestCase, TestCase.size()));
-	}
 	system("CLS");
-	for (int k = 0;k < noTestCases;k++) {
-		cout << output[k] << endl;
+	int h=0;
+	while (h < t) {
+		cout << output[h] << endl;
+		h++;
 	}
+	return 0;
 }
 
