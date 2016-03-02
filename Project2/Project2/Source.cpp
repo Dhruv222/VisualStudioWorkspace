ee7d100486eafbeeacdafe21b1d87918b3980cf8
Dhruv Sharma <dhruv004@e.ntu.edu.sg>
2016-03-03 01:45:33 +0530

    GregorianCalendar_CodeChef_Project2

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -1,50 +1,51 @@
 #include<iostream>
-#include<vector>
 #include<string>
-#include<stdlib.h>
 
 using namespace std;
 
 int main()
 {
-	int t,n,i=0,count=0;
-	string chef, correct;
-	vector<int> winning, output;
+	int t, year,count=0;
+	string weekday[7] = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+	int base_year = 2001;
+	string base_day = weekday[0];
 	cin >> t;
 	int counter = 0;
-	output.resize(t);
-	while (counter<t) {
+
+	while (counter < t) {
+		cin >> year;
 		count = 0;
-		cin >> n;
-		winning.clear();
-		winning.resize(n + 1);
-		cin >> correct;
-		cin >> chef;
-		for (i = 0;i < n + 1;i++) {
-			cin >> winning[i];
+		if (year == 2001) {
+			cout << weekday[0] << endl;
 		}
-		for (i = 0;i < n;i++) {
-			if (chef[i] == correct[i])
-				count ++;
-		}
-		if (count == n) {
-			output[counter] = winning[count];
+		else if (year < 2001) {
+			for (int i = year;i < 2001;i++){
+				if (i % 400 == 0)
+					count += 2;
+				else if (i % 100 == 0)
+					count += 1;
+				else if (i % 4 == 0)
+					count += 2;
+				else
+					count += 1;
+			}
+			cout << weekday[6-(count-1)%7]<<endl;
 		}
 		else {
-			i = 0;
-			output[counter] = winning[count];
-			while (i < count) {
-				if (winning[i]>output[counter])
-					output[counter] = winning[i];
-				i++;
+			for (int i = 2001;i < year;i++) {
+				if (i % 400 == 0)
+					count += 2;
+				else if (i % 100 == 0)
+					count += 1;
+				else if (i % 4 == 0)
+					count += 2;
+				else
+					count += 1;
 			}
-			
-		}
+			cout << weekday[count%7];
+			}
 		counter++;
-	}
-	for (i = 0;i < t;i++) {
-		cout << output[i] << endl;
-	}
+		}
 	return 0;
-}
+	}
 
